683a0afe289c74afc1467f4d1552d9567fe702a7
finpama <phma020@gmail.com>
2022-02-03 17:49:36 -0300

    Instalando Reactstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Home } from "./views/Home";
+import { ListarClientes } from "./views/Cliente/Listar";
+import { ListarPedidos } from "./views/Pedido/Listar";
+import { ListarServicos } from "./views/Servico/Listar";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={ Home } />
+          <Route path="/listar-clientes" component={ ListarClientes } />
+          <Route path="/listar-pedidos" component={ ListarPedidos } />
+          <Route path="/listar-servicos" component={ ListarServicos } />
         </Switch>
       </Router>
     </div>
