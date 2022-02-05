2f50c76eada119ce1df3d8a7c07cfe65fa5e004d
finpama <phma020@gmail.com>
2022-02-05 17:23:43 -0300

    Request de Items de Serviços

---
+++ src/views/Servico/Servico_ItensServicos/index.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Alert, Container, Table } from "reactstrap";
+
+import { api } from "../../../config";
+
+export const ListarItensServicos = (props) => {
+    console.clear()
+
+    const [id, setId] = useState(props.match.params.id)
+
+    const [data, setData] = useState([]);
+
+    const [status, setStatus] = useState({
+        type: '',
+        message: ''
+    });
+
+    
+    useEffect(() => {
+        const getItems = async () => {
+            await axios.get(`${api}/servico/${id}/pedidos`)
+                .then(response => {
+                    setData(response.data.itemPedido);
+                })
+                .catch(err => {
+                    setStatus({
+                        type: 'error',
+                        message: 'Sem conexão com o Servidor'
+                    })
+                    console.log(err);
+                });
+        }
+        
+        getItems();
+    }, [id]);
+
+    return (
+        <div>
+            <Container className="mt-3">
+                <div className="d-flex justify-content-between">
+                    <h1>Informações dos Itens</h1>
+                    <Link className="btn btn-primary m-1" to={'/listar/servicos/'}>Voltar</Link>
+                </div>
+                {status.type === 'error' ?
+                    <Alert color="danger">
+                        {status.message}
+                    </Alert> : ''
+                }
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <th>PedidoId</th>
+                            <th>ServiçoId</th>
+                            <th>Quantidade</th>
+                            <th>Valor R$</th>
+                            <th>Ação</th>
+                        </tr>
+                    </thead>
+                    <tbody>{
+                        data.map(item => {
+                            return (
+                                <tr key={item.PedidoId}>
+                                    <th>{item.PedidoId}</th>
+                                    <th>{item.ServicoId}</th>
+                                    <td>{item.quantidade}</td>
+                                    <td>{item.valor === null ? 0 : item.valor}</td>
+                                    <td className="text-center">
+                                        {/* <Link className="btn btn-outline-primary" to={'/listar/itens-servico/' + item.id}>Pedidos</Link> */}
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </Table>
+            </Container>
+        </div>
+    );
+}
